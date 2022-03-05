384975b670aa23b4314c8abe34b086bb09dc8132
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-05 20:35:28 +0300

    websocket connection errors fixed

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const express = require("express");
+const cors = require("cors");
 const socketio = require("socket.io");
 const http = require("http");
 const router = require("./router");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+app.use(cors());
+app.use(router);
 
-io.on("connection", (socket) => {
-  console.log("we have a new connection!!!");
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-  socket.on("participant", ({ name, room }, callback) => {
-    console.log(name, room);
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to room ${user.room} ${user.id}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
