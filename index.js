8c72f15c77d253436105fca76c9cb15243fe1996
Kerymster <kerimyilmaz5270@gmail.com>
2022-03-06 14:09:24 +0300

    server side completed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on("connect", (socket) => {
 
     socket.emit("message", {
       user: "admin",
-      text: `${user.name}, welcome to room ${user.room} ${user.id}.`,
+      text: `${user.name}, welcome to room "${user.room}".`,
     });
     socket.broadcast
       .to(user.room)
@@ -47,12 +47,21 @@ io.on("connect", (socket) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
-
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("User left!!!");
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left.`,
+      });
+    }
   });
 });
 
